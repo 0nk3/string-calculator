f587621859761b8f75e27bd46bef5e2dba94a3ee
Ndumiso Onke Fanti <0nk3.0nk3@gmail.com>
2019-12-06 13:38:16 +0200

    string calculator with the 3 basic methods taking 0, 1 or 2 parameters

--- src/main/java/Stringcalculator.java
+++ src/main/java/Stringcalculator.java
@@ -1,2 +1,20 @@
 public class Stringcalculator {
+
+    public int Add() {
+        return 0;
+    }
+
+    public int Add(String num1) {
+
+        if (num1.equals("")) {
+            return 0;
+        } else {
+            return Integer.parseInt(num1);
+        }
+    }
+    public int Add(String num1, String num2) {
+        return Integer.parseInt(num1) + Integer.parseInt(num2);
+    }
+
+
 }
