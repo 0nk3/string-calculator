8389e12c42736ab3415e715d253f0d29a84715f8
Ndumiso Onke Fanti <0nk3.0nk3@gmail.com>
2020-01-17 12:41:19 +0200

    add exception handling

--- src/main/java/Stringcalculator.java
+++ src/main/java/Stringcalculator.java
@@ -3,38 +3,45 @@ import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
 public class Stringcalculator {
-    public int Add(String stringOfNums){
+
+    public int Add(String stringOfNums) throws Exception {
         int sum = 0;
-        //data extracted using regex will be stored on this array list
-        ArrayList<String> matchList = new ArrayList<>();
+        //
+        if(stringOfNums.isEmpty()){
+            return 0;
+        }
+        else if(stringOfNums.length()==2){
+            String[] numbers = stringOfNums.split(",");
+            return Integer.parseInt(numbers[0]) + Integer.parseInt(numbers[1]);
+        }else{
+            //data extracted using regex will be stored on this array list
+            ArrayList<String> matchList = new ArrayList<>();
 
-        try {
-
+            try {
 //            Pattern patternDel = Pattern.compile("^(\\/\\/\\d+)");
 //            Matcher DelMatcher = patternDel.matcher(stringOfNums);
 //            stringOfNums = stringOfNums.replaceAll(String.valueOf(DelMatcher),"\\");
 
-            final Pattern pattern = Pattern.compile("\\d+");
-            final Matcher regexMatcher = pattern.matcher(stringOfNums);
 
-            while (regexMatcher.find()) {
-                matchList.add(regexMatcher.group());
-            }
+                final Pattern pattern = Pattern.compile("\\d+");
+                final Matcher regexMatcher = pattern.matcher(stringOfNums);
 
-        }catch (Exception e){
-                e.printStackTrace();
-        }finally {
-            for (String num: matchList) {
-                sum += Integer.parseInt(num);
+                while (regexMatcher.find()) {
+                    matchList.add(regexMatcher.group());
+                }
+
+            }catch (Exception e){
+                throw new Exception("Doesn't accept negative numbers");
+            }finally {
+                for (String num: matchList) {
+                    sum += Integer.parseInt(num);
+                }
             }
+            return sum;
         }
-        return sum;
     }
     public int Add(){
         return 0;
     }
 
-    public int Add(String num1, String num2) {
-        return Integer.parseInt(num1) + Integer.parseInt(num2);
-    }
 }
--- src/test/java/StringcalculatorTest.java
+++ src/test/java/StringcalculatorTest.java
@@ -14,43 +14,43 @@ class StringcalculatorTest {
     /* ***************************************************************************************** */
     //double quotation passed as argument
     @Test
-    void  emptyStringTest(){
+    void  emptyStringTest() throws Exception {
         assertEquals(0, stringcalculator.Add(""));
     }
     //Test for multiple numbers passed as string argument
     @Test
-    void  oneStringTest(){
+    void  oneStringTest() throws Exception {
         assertEquals(1, stringcalculator.Add("1"));
     }
 
     @Test
-    void twoStringTest(){
+    void twoStringTest() throws Exception {
         assertEquals(2, stringcalculator.Add("1,1"));
     }
     @Test
-    void multipleStringTest1(){
+    void multipleStringTest1() throws Exception {
         assertEquals(10, stringcalculator.Add("1,2,3,4"));
     }
     @Test
-    void multipleStringTest2(){
+    void multipleStringTest2() throws Exception {
         assertEquals(10, stringcalculator.Add("1,2,3,4"));
 
     }
     /* ******************************************************************************************* */
     // handling newlines test
     @Test
-    void testNewlines(){
+    void testNewlines() throws Exception {
         assertEquals(6,stringcalculator.Add("1\n2,3"));
     }
     /* ******************************************************************************************* */
     // Handling different delimiters
     @Test
-    void multipleStringTestWithDelimiters(){
+    void multipleStringTestWithDelimiters() throws Exception {
         assertEquals(3, stringcalculator.Add("//;\n1;2"));
     }
     //TODO : Needs to be fixed (stuck)
     @Test
-    void multipleStringTestWithDelimiters2(){
+    void multipleStringTestWithDelimiters2() throws Exception {
         assertEquals(146, stringcalculator.Add("//4\n142"));
     }
 }
