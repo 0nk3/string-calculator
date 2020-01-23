4fd5008c13dca71a31a63d109a8e9b5007a677cf
Ndumiso Onke Fanti <0nk3.0nk3@gmail.com>
2020-01-23 09:41:35 +0200

    Final

--- src/main/java/Main.java
+++
@@ -1,6 +0,0 @@
-public class Main {
-    public static void main(String[] args) throws Exception {
-        Stringcalculator str = new Stringcalculator();
-        System.out.println(str.Add("-1,-2,3,4"));
-    }
-}
